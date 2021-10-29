3eacebc8146ae5e67395d7ae0575bfc229d2d98f
marcocarmonaga <marco.carmonag@alumno.buap.mx>
2021-10-28 22:12:47 -0500

    new

--- MoB_2_project/Docs/Python_files/P_V_inverted.py
+++ MoB_2_project/Docs/Python_files/P_V_inverted.py
@@ -2,22 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 V0 = 178.3437592
-E0 = -4099.369449
 B0 = 0.1024416633e-01
 B0_prime = 4.190756356
-
+c0_a0 = 1.0958578897109787
 
 def P(V):
     return (3 * B0 / 2) * ((V0 / V)**(7 / 3)-(V0 / V)**(5 / 3))*(1+(3 / 4)*(B0_prime - 4)*((V0 / V)**(2 / 3) - 1))
 
-def c_a(V):
-    return (c / a) * (V0) + alpha(1-V/V0) +beta(1-V/V0)^2,
+def beta(c_a,V,alpha):
+    return ((((c_a)-(c0_a0))/(1-(V / V0)))-alpha)/(1-(V / V0))
 
+df = np.loadtxt('/home/marco/Documents/Elk_projects/MoB_2_project/c_a_vs_Energy/fits/v_vs_c_a_vs_energy.txt')
 
-volumes = [159.30, 194.70, V0, 162.84, 169.92, 184.08,
-           177.00, 173.46, 180.54, 191.16, 187.62, 166.38]
-
-pressure_points = [P(volume) for volume in volumes]
-
-for volume, pressure_point in zip(volumes, pressure_points):
-    print(volume, pressure_point)
+print(df)
